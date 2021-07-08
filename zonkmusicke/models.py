2f593d48e4e022bc195c2bbf76497c99ac0777e6
Muia23 <wilfred.muema@gmail.com>
2021-07-08 08:17:50 +0300

    8/8/2021 - UX updates

--- zonkmusicke/models.py
+++ zonkmusicke/models.py
@@ -3,8 +3,8 @@ from django.db.models.fields import BLANK_CHOICE_DASH
 
 # Create your models here.
 class HomeLanding(models.Model):
-    landingbackground = models.FileField(upload_to = 'home')
-    landinglogo = models.FileField(upload_to = 'home')
+    landingbackground = models.FileField(upload_to = 'media')
+    landinglogo = models.FileField(upload_to = 'images')
     landingheading = models.CharField(max_length=100)
     landingtext = models.TextField()
 
@@ -14,7 +14,7 @@ class HomeLanding(models.Model):
         return homland
 
 class HomeAbout(models.Model):
-    aboutimg = models.FileField(upload_to = 'home')
+    aboutimg = models.FileField(upload_to = 'media')
     abouttitle = models.CharField(max_length= 60)
     aboutpara1 = models.TextField()    
     aboutpara2 = models.TextField()    
