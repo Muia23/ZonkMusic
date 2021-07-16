437943597f337909706ee723d9a0f7fc208d83be
Wilfred Muema <64227473+Muia23@users.noreply.github.com>
2021-07-16 17:16:28 +0300

    More changes to settings

--- zonkmusic/settings.py
+++ zonkmusic/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'zonkmusic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if config('MODE')=="dev":
+if MODE=="dev":
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
